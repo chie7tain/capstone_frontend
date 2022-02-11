280e5c9f70f951ab78ddf7a8ba63410a8cb4e036
ejioguvictor <ejioguvictor6054@gmail.com>
2022-02-11 15:43:55 +0100

    updated sass module in tsconfig.json file

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import React from "react";
 import "./App.css";
+import LoginPage from "./components/LoginPage";
 
 function App() {
-  return <div className="App">Hello team</div>;
+  return (
+    <div>
+      <LoginPage />
+    </div>
+  );
 }
 
 export default App;
