1b5a51ca25a931aeeb39076cda4d956dfc20be53
nwagu chima <chima.nwagu@decagon.dev>
2022-02-21 17:35:03 +0100

    cleanup

--- src/Componets/MainContent/Message/AudioM/AudioMessage.tsx
+++ src/Componets/MainContent/Message/AudioM/AudioMessage.tsx
@@ -1,37 +1,42 @@
+import { useEffect, useRef, useState } from "react";
 import styles from "./AudioMessage.module.scss";
-import WaveSurfer from "wavesurfer.js";
+import WaveSurfer from "react-wavesurfer.js";
 
 const AudioMessage = () => {
+  //   const waveformRef = useRef(null);
+  //   const [position, setPosition] = useState(0);
+  //   const [muted, setMuted] = useState(false);
+  //   console.log(waveformRef);
+
+  //   useEffect(() => {
+  //     if (waveformRef.current) {
+  //       const wavesurfer = WaveSurfer.create({
+  //         container: waveformRef.current,
+  //       });
+  //     }
+  //   }, []);
+
+  //   return <div ref={waveformRef}></div>;
+
+  const [position, setPosition] = useState(0);
+  const [muted, setMuted] = useState(false);
+
+  const handlePositionChange = (position: number) => {
+    /* ... */
+  };
+
+  const onReadyHandler = () => console.log("done loading!");
+
   return (
-    <div className={styles.video}>
-      <video controls autoPlay>
-        <source src="" type="video/mp4" />
-      </video>
-      <div className={styles.vidText}>
-        <p>This is the video I Promised to send</p>
-        <span>12:35pm</span>
-      </div>
-    </div>
+    <WaveSurfer
+      src="https://file-examples-com.github.io/uploads/2017/11/file_example_MP3_700KB.mp3"
+      //   position={position}
+      onPositionChange={handlePositionChange}
+      onReady={onReadyHandler}
+      //   playing={playing}
+      //   muted={muted}
+    />
   );
 };
 
 export default AudioMessage;
-
-// export default function Chat(){
-//   const waveformRef = useRef();
-
-//   useEffect(() => {
-//     if(waveformRef.current) {
-//       const wavesurfer = WaveSurfer.create({
-//         container: waveformRef.current,
-//       });
-//     }
-//   }, []);
-
-//   return(
-//     <>
-//       <div ref={waveformRef}>
-//       </div>
-//     </>
-//   )
-//  }
