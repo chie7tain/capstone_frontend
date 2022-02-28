6ff90d114ea4507038aa189a64e5c14bbc9977b6
anita-brown <anita.osagie@decagon.dev>
2022-02-28 11:26:38 +0100

    commit before develop pull

--- src/components/Profile/ProfileLeftBar.tsx
+++ src/components/Profile/ProfileLeftBar.tsx
@@ -1,40 +1,62 @@
-import styles from './ProfileContainer.module.scss'
-import Frame from '../../assets/Frame.png'
-import {BiArrowBack, BiCamera} from 'react-icons/bi';
-
+import styles from "./ProfileContainer.module.scss";
+import Frame from "../../assets/Frame.png";
+import { BiArrowBack, BiCamera } from "react-icons/bi";
 
 const ProfileLeftBar = () => {
-    return (
-
-        <div className={styles['leftbar-container']}>
-        <div className={styles['leftbar-text']}>
-        <p>< BiArrowBack/></p>
+  return (
+    <div className={styles["leftbar-container"]}>
+      <div className={styles["leftbar-text"]}>
+        <p>
+          <BiArrowBack />
+        </p>
         <h2>Profile</h2>
-            </div>
-            
+      </div>
 
-        <div className={styles['profile-pic']}> 
-        
-            <p className={styles['icon']}><BiCamera/></p>
-        </div>
-            <div className={styles['profile-text']}>
-            {/* <input type="" placeholder=""/> */}
+      <div className={styles["profile-pic"]}>
+        {/* <i className={styles["icon"]}> */}
+        {/* <input type="file" /> */}
+        <label htmlFor="file-input">
+          <i className={styles["icon"]}>
+            <BiCamera />
+          </i>
+        </label>
+        <input type="file" className={styles["icon"]} />
 
-                <p className={styles['profile-name']}>Your name</p>
-            <img src={Frame} alt="" />
-            </div>
-            <div className={styles['profile-paragraph']}>
-
+        {/* </i> */}
+      </div>
+      <div className={styles["profile-text"]}>
+        <input
+          className={styles["profile-input"]}
+          type="text"
+          name=""
+          id=""
+          placeholder="Your name"
+        />
+        {/* <input type="" placeholder=""/> */}
+        {/* <p>Your name</p> */}
+        <i>
+          <img src={Frame} alt="" />
+        </i>
+      </div>
+      <div className={styles["profile-paragraph"]}>
         <h4>Shashank Mahajan</h4>
-        <p>This is not your username or pin. This name will be visibile to your WhatsApp contacts.</p>
-            </div>
-        <div className={styles['profile-text']}>
-                <p className={styles['profile-name']}>About</p>
-                <img src={Frame}alt="icon" />
-        </div>
-        <p className={styles['paragraph-primary']}>Notification off, please expect delayed revert. Call if very urgent.</p>
-  </div>
-        )
- }
+        <p>
+          This is not your username or pin. This name will be visibile to your
+          WhatsApp contacts.
+        </p>
+      </div>
+      <div className={styles["profile-text"]}>
+        <p className={styles["profile-name"]}>About</p>
+        <i>
+          <img src={Frame} alt="" />
+        </i>
+      </div>
+      <input className={styles["profile-input"]} type="text" name="" id="" />
+      <p className={styles["paragraph-primary"]}>
+        Notification off, please expect delayed revert. Call if very urgent.
+      </p>
+    </div>
+  );
+};
 
- export default ProfileLeftBar;
+export default ProfileLeftBar;
